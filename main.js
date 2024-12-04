ce72b4a5a0cea69dd7bdd40df1bf7356c601961e
Sabal11 <labasking4@gmail.com>
2024-12-04 12:12:36 +0545

    home 70%

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      alert('Thank you for your message! I will get back to you soon.');
-      form.reset();
-    });
-  });
